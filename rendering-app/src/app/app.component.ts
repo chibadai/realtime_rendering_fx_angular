8bee7d2afc39b9b66aca88a270675e89d5d3c678
Daisuke Chiba <92.ch.07.b.17.dai@gmail.com>
2023-11-23 15:13:52 +0900

    #3 AngularのSubscribe処理をflaskのURLと連動させた
    - Angularのappにsubscribeを実装
    - Subscribeに必要なserviceの実装
    - 動作検証済み
    - flaskの処理は別途実装

--- rendering-app/src/app/app.component.ts
+++ rendering-app/src/app/app.component.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+import { SseService } from './sse.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  template: `<div>...</div>`
 })
-export class AppComponent {
-  title = 'rendering-app';
+export class AppComponent implements OnInit, OnDestroy {
+  // private sseSubscription: Subscription;
+   private sseSubscription: Subscription= new Subscription()
+
+  constructor(
+    private sseService: SseService
+    // private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    // this.sseSubscription = this.sseService.getServerSentEvent('http://your-server-url/stream')
+    // this.sseSubscription = this.sseService.getServerSentEvent('http://127.0.0.1:5000/stream')
+    // this.sseSubscription = this.http.get('http://127.0.0.1:5000/stream', { responseType: 'text' })
+    this.sseSubscription = this.sseService.getServerSentEvent('http://127.0.0.1:5000/stream')
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+          // ここで受信したデータを扱います
+        },
+        error: (error) => console.error(error),
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.sseSubscription) {
+      this.sseSubscription.unsubscribe();
+    }
+  }
 }
+// import { Component } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [CommonModule, RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.sass'
+// })
+// export class AppComponent {
+//   title = 'rendering-app';
+// }
---
+++ rendering-app/src/app/sse.service.ts
@@ -0,0 +1,40 @@
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class SseService {
+
+//   constructor() { }
+// }
+import { Injectable, NgZone } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SseService {
+  constructor(private zone: NgZone) {}
+
+  getServerSentEvent(url: string): Observable<any> {
+    return new Observable(observer => {
+      const eventSource = new EventSource(url);
+
+      eventSource.onmessage = event => {
+        this.zone.run(() => {
+          observer.next(event);
+        });
+      };
+
+      eventSource.onerror = error => {
+        this.zone.run(() => {
+          observer.error(error);
+        });
+      };
+
+      return () => {
+        eventSource.close();
+      };
+    });
+  }
+}
